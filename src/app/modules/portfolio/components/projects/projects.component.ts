3ee6e965a60a516cbb1413b86362e9745fe17fae
Adriano Leite da Silva <adrianoleitedasilva@outlook.com>
2026-03-27 18:45:44 -0300

    update: finalizando o projeto

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,14 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
+
+// Interface
 import { IProjects } from '../../interface/IProjects.interface';
 
+// Angular Material
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+
+// Enum
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+
+// Dialog
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
+
   public arrayProjects = signal<IProjects[]>([
     {
       src: 'assets/img/adriano.jpg',
@@ -16,7 +28,8 @@ export class ProjectsComponent {
       title: 'Projeto',
       width: '100px',
       height: '51px',
-      description: '<p></p>',
+      description:
+        '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis ea alias, porro dolor quo provident cum tempore veniam? Eligendi quibusdam delectus cum recusandae, perspiciatis illum ipsum necessitatibus ratione beatae quas!</p>',
       links: [
         { name: 'Conheça o Blog', href: 'https://www.adrianosilva.pro.br/' },
       ],
@@ -27,7 +40,8 @@ export class ProjectsComponent {
       title: 'Projeto',
       width: '100px',
       height: '51px',
-      description: '<p></p>',
+      description:
+        '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis ea alias, porro dolor quo provident cum tempore veniam? Eligendi quibusdam delectus cum recusandae, perspiciatis illum ipsum necessitatibus ratione beatae quas!</p>',
       links: [
         { name: 'Conheça o Blog', href: 'https://www.adrianosilva.pro.br/' },
       ],
@@ -38,10 +52,18 @@ export class ProjectsComponent {
       title: 'Projeto',
       width: '100px',
       height: '51px',
-      description: '<p></p>',
+      description:
+        '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis ea alias, porro dolor quo provident cum tempore veniam? Eligendi quibusdam delectus cum recusandae, perspiciatis illum ipsum necessitatibus ratione beatae quas!</p>',
       links: [
         { name: 'Conheça o Blog', href: 'https://www.adrianosilva.pro.br/' },
       ],
     },
   ]);
+
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS,
+    });
+  }
 }
